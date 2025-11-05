3d7293f11ba054fb3a2855d89d56ac5df4b45888
pv_pial <kimphong90@hotmail.com>
2025-11-05 14:07:19 -0600

    only sign-in can view security page

--- src/app/(auth)/account/security/page.tsx
+++ src/app/(auth)/account/security/page.tsx
@@ -1,7 +1,15 @@
+import { headers } from "next/headers"
 import { SecuritySettingsCards } from "@daveyplate/better-auth-ui"
 import BackButton from "~/components/account/back-button"
+import { auth } from "~/lib/auth"
+import { redirect } from "next/navigation"
 
-export function SecuritySettings() {
+export default async function SecuritySettings() {
+    const session = await auth.api.getSession({ headers: await headers() })
+
+    if (!session) {
+        redirect("/auth/sign-in")
+    } 
     return (
         <div className="container mx-auto p-6">
             <BackButton fallback="/account/settings" />
@@ -10,5 +18,3 @@ export function SecuritySettings() {
         </div>
     )
 }
-
-export default SecuritySettings
